e1a77d69df30e2fb4c13b7bc0a02d11afee3798f
Azhar <azharumi786@gmail.com>
2022-09-07 22:13:26 +0530

    added reverse number

---
+++ ReverseNumber.java
@@ -0,0 +1,15 @@
+package com.bl.logicalprograms;
+
+public class ReverseNumber {
+	public static void main(String[] args) {
+		int num = 123456789;
+		int reverse = 0;
+
+		while (num != 0) {
+			int remainder = num % 10;
+			reverse = reverse * 10 + remainder;
+			num = num / 10;
+		}
+		System.out.println("The reverse of give number is: " + reverse);
+	}
+}
